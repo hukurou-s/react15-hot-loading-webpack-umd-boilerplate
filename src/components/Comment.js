1ddf0cf82aaa810f6560414fc8544e3af44a8a24
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-05-26 17:52:05 +0900

    Implements Function to Display Markdown

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import Remarkable from 'remarkable'
 
 export default class Comment extends React.Component{
+
+    rawMarkup() {
+	const md = new Remarkable();
+	const rawMarkup = md.render(this.props.children.toString(), {sanitize: true})
+	return { __html:rawMarkup };
+    }
+    
     render() {
+	const md = new Remarkable();
 	return (
 	    <div className="comment">
 		<h2 className="commentAuthor">
-                     {this.props.author}
-                </h2>
-		{this.props.children}
+                {this.props.author}
+            </h2>
+		<span dangerouslySetInnerHTML={this.rawMarkup()} />
 	    </div>
 	);
     }
